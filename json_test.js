c51931d297cb19ea3e20f5d7f44e7bb7c7c241f1
Salvador Soto <salvadorsoto9@gmail.com>
2021-02-04 19:30:27 -0800

    Made the simple web page and javascript File
    Index.html for the web page and displaying our JSON data, did this with javascript

---
+++ json_test.js
@@ -0,0 +1,30 @@
+const student = {
+    "name": "Sal",
+    "age": 20,
+    "occupation": "Student"
+}
+
+const studentToString = JSON.stringify(student);
+console.log(typeof studentToString);
+
+const studentToJSON = JSON.parse(studentToString);
+console.log(typeof studentToJSON);
+
+//Displaying our JSON on the HTML web page
+var mainContainer = document.getElementById("myData");
+
+var div = document.createElement("div");
+var div2 = document.createElement("div2");
+var div3 = document.createElement("div");
+
+div.innerHTML = 'Name: ' + studentToJSON.name;
+mainContainer.appendChild(div);
+
+div2.innerHTML = 'Age: ' + studentToJSON.age;
+mainContainer.appendChild(div2);
+
+div3.innerHTML = 'Occupation: ' + studentToJSON.occupation;
+mainContainer.appendChild(div3);
+
+
+
